848b85047323dd816cf13a9730f6702b7a71760c
Juan Rojas <jsrojasn@unal.edu.co>
2018-09-01 20:42:52 -0500

    Selector and title of the page fixed

--- src/selectors/posts.js
+++ src/selectors/posts.js
@@ -1,13 +1,21 @@
 const selectPosts = (posts, {filterBy, text})=>{
-    return posts.filter((post)=>{
+    const searchPosts = posts.filter((post)=>{
         return post.title.toLowerCase().includes(text)
-    }).sort((a,b)=>{
-        if (filterBy === "date") {
-           return  a.createdAt < b.createdAt ? 1 : -1
-        } else if ( filterBy === "title") {
-            return a.title < b.title ? -1:1
-        }
     })
+    if (filterBy ==="date") {
+        return searchPosts.sort((a,b)=>{
+            return a.createdAt<b.createdAt?1:-1
+        })
+    } else if (filterBy==="title"){
+        return searchPosts.sort( (a, b)=> {
+            const x = a.title.toLowerCase();
+            const y = b.title.toLowerCase();
+            if (x < y) { return -1; }
+            if (x > y) { return 1; }
+            return 0;
+        }); 
+    }
+    
 }
 
 export default selectPosts
